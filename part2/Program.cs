898515efab1651c3e729034f28e59853fb0fd235
dore43@office.deu.ac.kr <dore43@office.deu.ac.kr>
2022-09-22 18:55:56 +0900

    Program.cs 수정

--- part2/Program.cs
+++ part2/Program.cs
@@ -10,6 +10,14 @@ namespace part2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("이름 나이 학교 주소를 입력하시오.");
+            string k = Console.ReadLine();
+            string [] km = k.Split(' ');
+            string name = km[0];
+            int age = int.Parse(km[1]);
+            string university = km[2];
+            string adress = km[3];
+            Console.WriteLine($"이름: {name}, 나이: {age} \n학교: {university}, 주소: {adress}");
         }
     }
 }
